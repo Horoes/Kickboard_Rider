26f573efd815db80a4f37eec030b73a2d996f784
Horoes <sumbio123@gmail.com>
2024-07-17 06:46:17 +0900

    맵 생성까지 들어감
     ( 풀타일 동물 랜덤 배치 아직... 코드 가독성을 높이고 싶어서 미룸)

--- Kickboard_Rider/Assets/Scripts/GameManager.cs
+++ Kickboard_Rider/Assets/Scripts/GameManager.cs
@@ -4,31 +4,106 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    public static GameManager Instance { get; private set; }
 
-    public static GameManager Instance;
+    [SerializeField]
+    private GameObject grassTilePrefab;
+    [SerializeField]
+    private GameObject roadTilePrefab;
+    [SerializeField]
+    private GameObject roadTile2Prefab;
+    [SerializeField]
+    private GameObject sandTilePrefab;
+
+    private List<GameObject> tilePrefabs;
+    private float zPosition = 0f;
+    private int initialTileCount = 40;
+    private Queue<GameObject> recentTiles;
+    private int consecutiveRoadCount = 0;
 
     private void Awake()
     {
+        // 싱글톤 인스턴스 설정
         if (Instance == null)
         {
             Instance = this;
+            DontDestroyOnLoad(gameObject);
         }
         else
         {
-            Debug.LogWarning("씬에 두개 이상의 게임 매니저가 존재합니다!");
             Destroy(gameObject);
+            return;
         }
     }
 
-    // Start is called before the first frame update
     void Start()
     {
-        
+        tilePrefabs = new List<GameObject>
+        {
+            grassTilePrefab,
+            roadTilePrefab,
+            sandTilePrefab
+        };
+
+        recentTiles = new Queue<GameObject>();
+
+        for (int i = 0; i < initialTileCount; i++)
+        {
+            GenerateTile();
+        }
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.T))
+        {
+            GenerateTile();
+        }
+    }
+
+    void GenerateTile()
+    {
+        GameObject selectedPrefab = SelectTilePrefab();
+
+        if (selectedPrefab == roadTilePrefab || selectedPrefab == roadTile2Prefab)
+        {
+            consecutiveRoadCount++;
+            if (consecutiveRoadCount > 1)
+            {
+                selectedPrefab = roadTile2Prefab;
+            }
+        }
+        else
+        {
+            consecutiveRoadCount = 0;
+        }
+
+        GameObject newTile = Instantiate(selectedPrefab, new Vector3(0, 0, zPosition), Quaternion.identity);
+        zPosition += 3f;
+
+        recentTiles.Enqueue(selectedPrefab);
+
+        if (recentTiles.Count > 2)
+        {
+            recentTiles.Dequeue();
+        }
+    }
+
+    GameObject SelectTilePrefab()
+    {
+        float randomValue = Random.Range(0f, 100f);
+
+        if (randomValue < 65f)
+        {
+            return roadTilePrefab;
+        }
+        else if (randomValue < 90f)
+        {
+            return grassTilePrefab;
+        }
+        else
+        {
+            return sandTilePrefab;
+        }
     }
 }
